4082d8d58ccb61046c9f1b9c7de21a152a3434b0
Christopher Lovell <clovell@student.42adel.org.au>
2023-06-05 20:15:35 +0930

    texture

--- src/sort/helm_sort.c
+++ src/sort/helm_sort.c
@@ -6,11 +6,10 @@
 /*   By: clovell <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 15:24:28 by clovell           #+#    #+#             */
-/*   Updated: 2023/06/01 14:17:00 by clovell          ###   ########.fr       */
+/*   Updated: 2023/06/05 19:18:56 by clovell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <limits.h>
-#include <stdlib.h>
 #include "sort.h"
 #include "stack.h"
 #include "pivot.h"
@@ -18,7 +17,8 @@
 /* Finds the amount of rotations
  * needed to bring the largest element to the top of the stack
  * Reverse rotations are values < 0
-
+ */
+/*
 static int	rotate_largest(t_stack *stack, t_node *next, int max, int forwards)
 {
 	int		curr;
@@ -46,6 +46,32 @@ static int	rotate_largest(t_stack *stack, t_node *next, int max, int forwards)
 	return (rotations);
 }
 */
+static int rotate_largest(t_stack *stack, t_node *next, int forwards)
+{
+	int	min;
+	int rotations;
+	int rotated;
+
+	min = INT_MIN;
+	rotations = 0;
+	rotated = 0;
+	while (next)
+	{
+		if (next->value > min)
+		{
+			min = next->value;
+			rotations = rotated;
+		}
+		if (forwards)
+			next = next->next;
+		else
+			next = next->prev;
+		rotated += 1 + ((forwards == 0) * -2);
+	}
+	if (rotations > (stack->count / 2))
+		return (rotate_largest(stack, stack->tail, 0));
+	return (rotations);
+}/*
 static int rotate_smallest(t_stack *stack, t_node *next, int forwards)
 {
 	int	min;
@@ -72,14 +98,14 @@ static int rotate_smallest(t_stack *stack, t_node *next, int forwards)
 		return (rotate_smallest(stack, stack->tail, 0));
 	return (rotations);
 }
-
+*/
 void	push_back(t_sort *sort)
 {
 	int	rotations;
 
 	while (sort->b->count > 0)
 	{
-		rotations = rotate_smallest(sort->b, sort->b->head, 1);
+		rotations = rotate_largest(sort->b, sort->b->head, 1);
 		while (rotations != 0)
 		{
 			if (rotations < 0)
@@ -110,6 +136,10 @@ int	get_smallest(t_stack *stack, int min)
 }*/
 void	stn_print(t_sort *sort);
 
+
+#include <unistd.h>
+
+
 /* Helm sort:
  * Sort stack 'A' using the following approach:
  * Let 'n' be the amount of elements in the stack
@@ -119,7 +149,7 @@ void	stn_print(t_sort *sort);
  */
 void	helm_sort(t_sort *sort)
 {
-	const int	quatre = sort->a->count / 4;
+	const int	quatre = (sort->a->count / 4) + 1;
 	int			pushed;
 	int			max;
 	int			min;
@@ -128,15 +158,14 @@ void	helm_sort(t_sort *sort)
 	t_median	*med;
 	int			place;
 
-	rotations = 0;
 	place = quatre;
 	med = create_median(sort->a);
-	pushed = 0;
-	max = get_smallest(med, quatre);
 	min = INT_MIN;
 	while (!is_sorted(sort))
 	{
-		stn_print(sort);
+		rotations = 0;
+		pushed = 0;
+		max = get_smallest(med, place);
 		while (rotations < sort->a->count && sort->a->count > 0)
 		{
 			if (sort->a->head->value <= max && sort->a->head->value >= min)
@@ -150,13 +179,17 @@ void	helm_sort(t_sort *sort)
 				rotations++;
 			}
 		}
-		rotations = 0;
-		pushed = 0;
-		place += quatre;
-		min = max;
-		max = get_smallest(med, place) + 1;
+		//stn_print(sort);
+		min = max + 1;
 		push_back(sort);
+		//n_print(sort);
+		while (pushed > 0)
+		{
+			op_rra(sort);
+			pushed--;
+		}
+		//stn_print(sort);
+		place += quatre;
+		sleep(4); // BAD FUNCTION, DO NOT USE.
 	}
-	free(med->array);
-	free(med);
 }
